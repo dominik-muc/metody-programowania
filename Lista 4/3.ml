9bffe7224a77d607f4ea480cc29cb67f550ee881
Dominik Muc <dominik.m.muc@gmail.com>
2024-03-25 19:22:23 +0100

    fix

--- Lista 4/3.ml
+++ Lista 4/3.ml
@@ -3,6 +3,7 @@ module MakeListDict (M: Map.OrderedType) : DICT with type key = M.t = struct
   type 'a dict = (key * 'a) list
   let empty = []
   let remove k d = List.filter (fun (k', _) -> k <> k') d
+  q
   let insert k v d = (k, v) :: remove k d
   let find_opt k d = List.find_opt (fun (k', _) -> k = k') d |> Option.map snd
   let find k d = List.find (fun (k', _) -> k = k') d |> snd
